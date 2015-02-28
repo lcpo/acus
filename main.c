9dcff40506886b41721e41cf693cde694cf3a3b7
S.S.Korotaev <lcpo.net@yandex.com>
2015-02-28 05:11:35 +0300

    2015-02-28_05:11:35

---
+++ main.c
@@ -0,0 +1,10 @@
+#include "suac.c"
+
+int main(int argc, char** argv,char** env){
+	sys_t s;
+	s=libs(s);
+
+s.write(0,"abcdef\n",7);
+
+return 0;	
+	}
---
+++ suac.c
@@ -0,0 +1,25 @@
+
+#include "suac.h"
+
+
+#ifndef __NO_START
+int _start(int argc){
+
+#ifdef __x86_64__	
+char** argv=(char**)&argc+5;
+#endif
+
+#ifdef __i386__
+char** argv=(char**)&argc;
+#endif
+int i=0;
+while(argv[i]!=0){i++;}
+argc=i;
+char** env=(char**)&argv[i+1];
+
+s=libs(s);
+
+s.exit(main(argc, argv,env));
+return 0;
+}	
+#endif
